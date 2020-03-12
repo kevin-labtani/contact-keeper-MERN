d1c6b499b5cead094c278c316c9a51bc96277954
Kevin Labtani <kevinlabtani@gmail.com>
2020-03-12 12:12:41 +0100

    connect to db - promise

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// connect to mongoDB
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the Contact Keeper API" })
@@ -12,6 +14,7 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}!`);
 });
