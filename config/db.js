5ef89bba777dc1f735ef6293e4a9d94523dddded
Kevin Labtani <kevinlabtani@gmail.com>
2020-03-12 12:14:27 +0100

    connect to db - async/await

--- config/db.js
+++ config/db.js
@@ -3,19 +3,20 @@ const config = require("config");
 
 const db = config.get("mongoURI");
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false
-    })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => {
-      console.error(err.message);
-      process.exit(1);
     });
+
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
