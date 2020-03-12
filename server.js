f3031d0b99d91059200c600c339cbd4ab8622da2
Kevin Labtani <kevinlabtani@gmail.com>
2020-03-12 11:58:23 +0100

    back-end routes

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.json({msg: "Welcome to the Contact Keeper API"}));
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the Contact Keeper API" })
+);
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}!`);
